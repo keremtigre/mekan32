0839b8c4c169acb417a38a8ed8f469db0fdd4c47
Kerem <keremtigre@hotmail.com>
2020-12-25 17:20:06 +0300

    versiyon-3

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,46 +1,44 @@
 var mongoose=require('mongoose');
-var dbURI='mongodb://localhost/mekan32';
-mongoose.connect(dbURI,{useNewUrlParser:true});
+var dbURI='mongodb+srv://mekan32:18812000@mekan32.jdciu.mongodb.net/mekan32?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true });
 
-//bağlandığımızda konsola bağlantı bilgilerini yazdırır.
-mongoose.connection.on('connected',function(){
-console.log('Mongoose '+dbURI+' adresindeki veritabanına bağlandı\n');
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose ' + dbURI +
+        ' adresindeki veritabanına bağlandı \n');
 });
 
-//bağlantı hatası olduğunda konsola hata bilgisini yazdıracak
-mongoose.connection.on('error',function(err){
-console.log('Mongoose bağlantı hatası\n: '+err);
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose bağlantı hatası \n: ' + err);
 });
 
-//bağlantı kesildiğinde konsola bağlantı kesildi bilgisini yazacak.
-mongoose.connection.on('disconnected',function(){
-console.log('Mongoose bağlantısı kesildi\n');
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose bağlantısı kesildi \n');
 });
 
-kapat=function(msg,callback){
-    mongoose.connection.close(function(){
-        console.log('Mongoose kapatıldı\n '+msg);
+kapat = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose kapatıldı \n ' + msg);
         callback();
     });
 };
-//nodemon için kapatma
-process.once('SIGUSR2', function(){
-    kapat('nodemon kapatıldı\n',function(){
+process.once('SIGUSR2', function () {
+    kapat('nodemon kapatıldı\n', function () {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
-//uygulama kapandığında kapatma
-process.on('SIGINT',function(){
-    kapat=('Uygulama kapatıldı\n',function(){
-process.exit(0);
+
+process.on('SIGINT', function () {
+    kapat('Uygulama kapatıldı\n', function () {
+        process.exit(0);
     });
 });
-//herokudan kapatma işlemi gerçekleşirse
-process.on('SIGTERM',function(){
-    kapat=('heroku kapatıldı\n',function(){
-process.exit(0);
+
+process.on('SIGTERM', function () {
+    kapat('Heroku kapatıldı\n', function () {
+        process.exit(0);
     });
 });
+
 require('./mekansema');
 
 
