d896bfb72bb08699b04a923d57f714b05b6f4bdd
Kerem <keremtigre@hotmail.com>
2020-12-25 16:23:54 +0300

    versiyon-3

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('./app_server/models/db');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname,'app_server','views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('ndev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
