3ccfd26bb291c357f00399bb145527af3c7d5b98
Kerem <keremtigre@hotmail.com>
2021-01-11 00:21:16 +0300

    "11. Hafta Ekelemleri"

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,100 +1,126 @@
-var express = require('express');
-var router = express.Router();
-const anaSayfa=function(req, res, next) {
+var request=require('postman-request');
+var apiSecenekleri={
+  sunucu: "http://localhost:3000",
+  apiYolu:'/api/mekanlar/'
+}
+var istekSecenekleri
+var mesafeyiFormatla=function(mesafe){
+  var yeniMesafe, birim;
+  if(mesafe>1000){
+      yeniMesafe=parseFloat(mesafe/1000).toFixed(2);
+      birim='km';
+  }
+  else{
+      yeniMesafe=parseFloat(mesafe).toFixed(1);
+      birim='m';
+  }
+  return yeniMesafe + birim;
+}
+var footer="Samed Kerem Tiğre 2020"
+var anaSayfaOlustur=function(req,res,cevap,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+      mesaj='API HATASI: Bir şeyler ters gitti';
+      mekanListesi =[];
+  }
+  else{
+      if(!mekanListesi.length){
+          mesaj="Civarda Herhangi Bir Mekan Bulunmadı!";
+      }
+  }
   res.render('mekanlar-liste',
-   { 
-     'baslik': 'Anasayfa',
-     'sayfaBaslik':{
-       'siteAd':'Mekan32',
-       'aciklama':'Isparta civarındaki mekanları keşfedin'
-     },
-     'footer':'Samed Kerem Tiğre 2020',
-     'mekanlar':[
-       {
-         'ad':'Starbucks',
-         'adres':'Iyaşpark AVM',
-         'puan':'3',
-         'imkanlar':['kahve','çay','pasta'],
-         'mesafe':'9km'
-       },
-       {
-         'ad':'Gloria Jeans',
-         'adres':'Iyaşpark AVM',
-         'puan':'3',
-         'imkanlar':['kahve','kek','pasta'],
-         'mesafe':'9km'
-         
-       },
-       {
-         'ad':'Köfteci Yusuf',
-         'adres':'Iyaş Bulvar AVM',
-         'puan':'4',
-         'imkanlar':['köfte','kahvaltı'],
-         'mesafe':'11km'
-       },
-       {
-         'ad':'Donas',
-         'adres':'Isparta Merkez Kafeler Caddesi',
-         'puan':'2',
-         'imkanlar':['tavuk döner','et döner','içecekler'],
-         'mesafe':'1km'
-       },
-       {
-         'ad':'Baydöner',
-         'adres':'İyaş Park',
-         'puan':'5',
-         'imkanlar':['tatlı','kebap','iskender'],
-         'mesafe':'5km'
-       }
-       
-     ]
+  {
+      baslik:'Mekan32',
+      sayfaBaslik:{
+          siteAd:'Mekan32',
+          aciklama:'Isparta Civarındaki Mekanları Keşfedin!'
+      },
+      footer:footer,
+      mekanlar:mekanListesi,
+      mesaj: mesaj,
+      cevap:cevap
+
+  });
+
+}
+const anaSayfa=function(req, res) {
+ istekSecenekleri={
+   url:apiSecenekleri.sunucu + apiSecenekleri.apiYolu,
+   method:"GET",
+   json:{},
+   qs:{
+     enlem:req.query.enlem,
+     boylam:req.query.boylam
    }
-   );
+ };
+ request(
+   istekSecenekleri,
+   function(hata,cevap,mekanlar){
+     var i,gelenMekanlar;
+     gelenMekanlar=mekanlar;
+if(!hata && gelenMekanlar.length){
+  for(i=0;i<gelenMekanlar.length;i++){
+    gelenMekanlar[i].mesafe=
+    mesafeyiFormatla(gelenMekanlar[i].mesafe);
+  }
+}
+anaSayfaOlustur(req,res,cevap,gelenMekanlar);
+
+   }
+ );
 }
 
-const mekanBilgisi=function(req, res) {
-  res.render('mekan-detay', { 
-    'baslik': 'Mekan Bilgisi',
-    'sayfaBaslik':'Starbucks',
-    'footer':'Samed Kerem Tiğre 2020',
-    'mekanBilgisi':{
-      'ad':'Starbucks',
-      'adres':'Iyaşpark AVM',
-      'puan':3,
-      'imkanlar':['Kahve','Pasta','Kek'],
-      'koordinatlar':{
-        'enlem':37.78253988714279,
-        'boylam':30.544915909008672
-      },
-      'saatler':[
-        {
-          'gunler':'Pazartesi-Cuma',
-          'acilis':'7:00',
-          'kapanis':'23:00',
-          'kapali':false
-        },
-        {
-          'gunler':'Cumartesi',
-          'acilis':'9:00',
-          'kapanis':'22:00',
-          'kapali':false
-        },
-        {
-          'gunler':'Pazar',
-          'kapali':true
-        }
-      ],
-      'yorumlar':[
-        {
-          'yorumYapan':'Samed Kerem Tiğre',
-          'puan':3,
-          'tarih':'01.12.2020',
-          'yorumMetni':'Kahveleri güzel beğeniyorum.'
-        }
-      ]
-    }
-    
+var detaySayfasiOlustur=function(req, res,mekanDetaylari){
+  res.render('mekan-detay',
+  {
+    baslik: mekanDetaylari.ad,
+    sayfaBaslik: mekanDetaylari.ad,
+    mekanBilgisi:mekanDetaylari
   });
+}
+
+var hataGoster=function(req, res,durum){
+  var baslik,icerik;
+  if(durum==404){
+    baslik="404, Sayfa Bulunamadı!";
+    icerik="Kusura bakma sayfayı bulamadık!"
+  }
+  else{
+    baslik=durum+", Birşeyler ters gitti!";
+    icerik="Ters giden bir şeyler var!";
+  }
+  res.status(durum);
+  res.render('hata',{
+baslik:baslik,
+icerik:icerik
+  });
+};
+
+const mekanBilgisi=function(req, res) {
+ istekSecenekleri={
+url: apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid,
+
+method:"GET",
+json:{}
+
+ };
+ request(
+   istekSecenekleri,
+   
+   function(hata, cevap,mekanDetaylari){
+     var gelenMekan=mekanDetaylari;
+
+     if(cevap.statusCode=200){
+       gelenMekan.koordinatlar={
+         enlem:mekanDetaylari.koordinatlar[0],
+         boylam:mekanDetaylari.koordinatlar[1]
+       };
+       detaySayfasiOlustur(req,res,gelenMekan);
+     } else{
+       hataGoster(req,res,cevap.statusCode);
+     }
+   }
+ );
 }
 
 const yorumEkle=function(req, res, next) {
